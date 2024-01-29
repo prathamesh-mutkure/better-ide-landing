82ce91e90da33d61c3508d3a4a197391789e71f5
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-01-30 00:37:13 +0530

    Navbar added

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,32 @@ function App() {
   return (
     <main className="relative bg-black text-white" data-theme="luxury">
       <div className="h-screen w-screen flex flex-col items-center justify-center">
-        <header></header>
+        <header className="w-full">
+          <div className="navbar bg-transparent w-full">
+            <div className="navbar-start">
+              <div className="dropdown">
+                <div
+                  tabIndex={0}
+                  role="button"
+                  className="btn btn-ghost lg:hidden"
+                >
+                  <img className="h-5 w-5" src="/vite.svg" />
+                </div>
+              </div>
+
+              <a className="btn btn-ghost text-xl hidden lg:flex">
+                <img className="h-10 w-10" src="/vite.svg" />
+              </a>
+            </div>
+
+            <div className="navbar-center hidden lg:flex"></div>
+
+            <div className="navbar-end gap-2">
+              <a className="btn">Better IDE</a>
+              <a className="btn">AO Notebook</a>
+            </div>
+          </div>
+        </header>
 
         <section
           id="hero"
@@ -16,8 +41,9 @@ function App() {
             </span>
           </h1>
 
-          <p className="text-gray-400 text-2xl">
-            Build fast. Ship fast. Using BetterIDEa.
+          <p className="text-gray-400 text-2xl capitalize">
+            Build fast. Ship fast.{" "}
+            <span className="hover:underline">Using BetterIDEa.</span>
           </p>
 
           <div className="flex flex-row justify-center items-center gap-5 pt-2">
@@ -27,15 +53,6 @@ function App() {
       </div>
     </main>
   );
-}
-
-{
-  /* <h1 className="item-start font-heading text-4xl">
-            <RotatingText
-              texts={["Build Fast", "Ship Fast", "Use BetterIDE"]}
-              period={1000}
-            />
-          </h1> */
 }
 
 export default App;
