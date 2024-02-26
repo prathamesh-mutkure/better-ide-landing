f9495b394839a608c4032ae85f49df31be510770
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-02-26 15:03:28 +0530

    Fix Build Errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Icons } from "./components/icons";
 import { type LucideIcon } from "lucide-react";
 import { cn } from "./lib/utils";
