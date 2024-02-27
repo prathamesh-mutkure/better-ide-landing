6a26292f48ee2daf14cc50cd99a2377968f3c615
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-02-27 10:37:49 +0530

    Teams Cards

--- src/Team.tsx
+++ src/Team.tsx
@@ -1,6 +1,38 @@
-import { Title } from "./App";
+import { SocialLink, Title } from "./App";
 import { Footer } from "./components/footer";
 import { Header } from "./components/header";
+import { Icons } from "./components/icons";
+import { cn } from "./lib/utils";
+
+const teamData: {
+  name: string;
+  position: string;
+  twitter: string;
+  photo: string;
+  top: number;
+}[] = [
+  {
+    name: "Farat Kadiwala",
+    position: "Principal Designer",
+    twitter: "https://twitter.com/prathamesh_io",
+    photo: "/team/farat.png",
+    top: 60,
+  },
+  {
+    name: "Prathamesh Mutkure",
+    position: "Backend and Architect",
+    twitter: "https://twitter.com/prathamesh_io",
+    photo: "/team/ankush.png",
+    top: 100,
+  },
+  {
+    name: "Prathamesh Mutkure",
+    position: "Frontend",
+    twitter: "https://twitter.com/prathamesh_io",
+    photo: "/team/prathamesh.png",
+    top: 60,
+  },
+];
 
 export function TeamPage() {
   return (
@@ -8,14 +40,31 @@ export function TeamPage() {
       <div className="h-screen w-screen flex flex-col items-center justify-center">
         <Header />
 
-        <section
-          id="hero"
-          className="relative flex flex-col text-center flex-auto justify-center items-center gap-16 h-screen w-screen"
-        >
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 container">
-            <Title>
-              Our <span className="text-[var(--green)]">team</span>
-            </Title>
+        <section id="hero" className="relative">
+          <Title className="text-center">
+            Our <span className="text-[var(--green)]">team</span>
+          </Title>
+
+          <div className="h-36"></div>
+
+          <div className="flex flex-row justify-center items-center gap-16">
+            {teamData.map(({ name, position, twitter, photo, top }) => (
+              <div className="flex flex-col gap-4 text-center items-center">
+                <div className="h-[260px] w-[240px] bg-white rounded-xl relative">
+                  <img
+                    src={photo}
+                    className={cn("w-full h-auto absolute", `top-[-${top}px]`)}
+                  />
+                </div>
+
+                <div>
+                  <p className="text-[22px]">{name}</p>
+                  <p className="text-[#686868] text-[18px]">{position}</p>
+                </div>
+
+                <SocialLink href={twitter} Icon={Icons.twitter} label="" />
+              </div>
+            ))}
           </div>
         </section>
       </div>
