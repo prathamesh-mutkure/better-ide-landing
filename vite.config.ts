410bf1d804d77802d1a68649d01f542d07025682
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-03-06 00:55:28 +0530

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePluginRadar } from "vite-plugin-radar"
+import { VitePluginRadar } from "vite-plugin-radar";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "",
-  plugins: [react(), VitePluginRadar({
-    analytics: [{ id: "G-QVLPJB3QT5" }]
-  })],
+  base: "/",
+  plugins: [
+    react(),
+    VitePluginRadar({
+      analytics: [{ id: "G-QVLPJB3QT5" }],
+    }),
+  ],
 });
